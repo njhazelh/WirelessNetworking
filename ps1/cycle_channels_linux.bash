99cc7ba106258eb84d46e682ca4768de812c5b79
Nick Jones <njhazelh@gmail.com>
2016-01-30 17:13:55 -0500

    Don't run cycle if not root

--- ps1/cycle_channels_linux.bash
+++ ps1/cycle_channels_linux.bash
@@ -15,6 +15,11 @@ function main() {
     local interface=$1
     local c
 
+    if [ "$(id -u)" != "0" ]; then
+        echo "This script must be run as root" 1>&2
+        exit 1
+    fi
+
     while true
     do
         for c in $(seq 1 13)

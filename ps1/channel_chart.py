d6f3a51bbb7f57736356f74f54384794a168475f
njhazelh <njhazelh@gmail.com>
2016-01-30 00:01:56 -0500

    Add basic matplot stuff for pt1

--- ps1/channel_chart.py
+++ ps1/channel_chart.py
@@ -3,6 +3,7 @@
 import matplotlib
 import sys
 import collections
+import numpy as np
 
 Beacon = collections.namedtuple("Beacon", "bssid channel dbm freq ssid")
 
@@ -53,6 +54,23 @@ def make_chart(info):
     info = sorted(info, key=lambda x: int(x.channel))
     for n in info:
         print(n)
+    data = np.array([b.channel for b in info])
+    N = len(data)
+    labels = ['point{0}'.format(i) for i in range(N)]
+    plt.subplots_adjust(bottom = 0.1)
+    plt.scatter(
+        data[:, 0], data[:, 1], marker = 'o', c = data[:, 2], s = data[:, 3]*1500,
+        cmap = plt.get_cmap('Spectral'))
+    for label, x, y in zip(labels, data[:, 0], data[:, 1]):
+        plt.annotate(
+            label,
+            xy = (x, y), xytext = (-20, 20),
+            textcoords = 'offset points', ha = 'right', va = 'bottom',
+            bbox = dict(boxstyle = 'round,pad=0.5', fc = 'yellow', alpha = 0.5),
+            arrowprops = dict(arrowstyle = '->', connectionstyle = 'arc3,rad=0'))
+
+    plt.show()
+
 
 
 def save_chart(chart):

06e548d6b22e29a7d5852d7205667df1f4a4710f
Nick Jones <njhazelh@gmail.com>
2016-01-30 20:56:15 -0500

    Add duration to fingerprint

--- ps1/part3/run_fingerprint.bash
+++ ps1/part3/run_fingerprint.bash
@@ -4,9 +4,11 @@ function run() {
 	local interface=$1
 	local room=$2
 
-	tshark -i "$interface" -X lua_script:record_beacons.lua 2>/dev/null | \
+	tshark -i "$interface" -a duration:10 -X lua_script:record_beacons.lua 2>/dev/null |
 		grep -oE $'[abcdef1234567890:]*\t[[:digit:]]*\t-?[[:digit:]]*$' |
-		./parse_beacons.py > "$room"
+		./parse_beacons.py > "fingerprints/$room"
+
+	echo "Wifi fingerprint generated in file: fingerprints/$room"
 }
 
 run "$1" "$2"

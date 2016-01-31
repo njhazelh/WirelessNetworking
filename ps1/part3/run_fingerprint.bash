3382632a0d6e289ef660572c89f9e5d0231d4684
Nick Jones <njhazelh@gmail.com>
2016-01-30 21:45:41 -0500

    Add fingerprints dir

--- ps1/part3/run_fingerprint.bash
+++ ps1/part3/run_fingerprint.bash
@@ -4,6 +4,9 @@ function run() {
 	local interface=$1
 	local room=$2
 
+	# create fingerprints dir if it doesn't exist
+	[ -d fingerprints ] || mkdir fingerprints
+
 	tshark -i "$interface" -a duration:10 -X lua_script:record_beacons.lua 2>/dev/null |
 		grep -oE $'[abcdef1234567890:]*\t[[:digit:]]*\t-?[[:digit:]]*$' |
 		./parse_beacons.py > "fingerprints/$room"

1e2b018b3288cf2f7785fe01b93283ddd2fea820
Nick Jones <njhazelh@gmail.com>
2016-01-30 19:07:05 -0500

    cleanup a little

--- ps1/part2/chart_averages.py
+++ ps1/part2/chart_averages.py
@@ -3,15 +3,15 @@
 import matplotlib.pyplot as plt
 import sys
 import collections
-import numpy as np
 
 Point = collections.namedtuple("Point", "channel dbm freq avg")
 dBms = []
 
+
 def split_line(line):
     parts = line.strip().split("\t")
     if len(parts) != 4 or "" in parts or "nil" in parts or parts[3] in dBms:
-       	print("csv formatting bad")
+        print("csv formatting bad")
         return None
     dBms.append(parts[3])
     return Point(channel=parts[0],
@@ -29,49 +29,42 @@ def read_lines():
         points.append(b)
     return points
 
-"""
-def reduce_lines(points):
-    info = {}
-    for p in points:
-        bssid = b.bssid
-        bssid_beacons = info.get(bssid) or {}
-        bssid_beacons_channel = bssid_beacons.get(b.channel) or []
-        bssid_beacons_channel.append(b)
-        bssid_beacons[b.channel] = bssid_beacons_channel
-        info[b.bssid] = bssid_beacons
 
-    avg_beacons = []
-    for ssid, network in info.items():
-        for channel, beacons in network.items():
-            dbms = [int(b.dbm) for b in beacons if b.dbm != "nil"]
-            avg_dbm = sum(dbms) / len(dbms)
-            avg_beacons.append(
-                Beacon(beacons[0].bssid, beacons[0].channel, avg_dbm,
-                       beacons[0].freq, beacons[0].ssid))
+# def reduce_lines(points):
+#     info = {}
+#     for p in points:
+#         bssid = b.bssid
+#         bssid_beacons = info.get(bssid) or {}
+#         bssid_beacons_channel = bssid_beacons.get(b.channel) or []
+#         bssid_beacons_channel.append(b)
+#         bssid_beacons[b.channel] = bssid_beacons_channel
+#         info[b.bssid] = bssid_beacons
 
-    return avg_beacons
-"""
+#     avg_beacons = []
+#     for ssid, network in info.items():
+#         for channel, beacons in network.items():
+#             dbms = [int(b.dbm) for b in beacons if b.dbm != "nil"]
+#             avg_dbm = sum(dbms) / len(dbms)
+#             avg_beacons.append(
+#                 Beacon(beacons[0].bssid, beacons[0].channel, avg_dbm,
+#                        beacons[0].freq, beacons[0].ssid))
+
+#     return avg_beacons
+
 
 def make_chart(points):
-    info = sorted(points, key=lambda x: int(x.dbm))
-    # data = np.array([(b.dbm, b.avg) for b in info])
-    data = []
-    for b in info:
-        data.append((b.dbm, b.avg))
+    data = [(b.dbm, b.avg) for b in points]
     plt.figure(figsize=(15, 10), dpi=100)
     plt.title("Average packets per second by RSSI")
     plt.xlabel("dBm")
     plt.ylabel("Average packets per second")
-    plt.rc('font', family='arial', weight='normal', size=8)
-    labels = [b.dbm for b in info]
     plt.scatter(*zip(*data))
-    plt.axis([-100, -40, 0, 300])
-    plt.savefig("averages.png")
+    plt.savefig("averages_scatter.png")
 
 
 def main():
     points = read_lines()
-    chart = make_chart(points)
+    make_chart(points)
 
 
 if __name__ == "__main__":

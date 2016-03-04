4343b06ad8d3a961e9525ca351a6ac66fc40bb13
njhazelh <njhazelh@gmail.com>
2016-03-04 16:20:49 -0500

    Minor changes to prob2

--- ps3/prob2/top_block.py
+++ ps3/prob2/top_block.py
@@ -3,7 +3,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Top Block
-# Generated: Fri Mar  4 15:46:46 2016
+# Generated: Fri Mar  4 16:17:20 2016
 ##################################################
 
 if __name__ == '__main__':
@@ -16,16 +16,22 @@ if __name__ == '__main__':
         except:
             print "Warning: failed to XInitThreads()"
 
+from gnuradio import analog
 from gnuradio import audio
 from gnuradio import blocks
 from gnuradio import eng_notation
 from gnuradio import filter
 from gnuradio import gr
+from gnuradio import wxgui
 from gnuradio.eng_option import eng_option
+from gnuradio.fft import window
 from gnuradio.filter import firdes
+from gnuradio.wxgui import fftsink2
 from gnuradio.wxgui import forms
 from grc_gnuradio import wxgui as grc_wxgui
 from optparse import OptionParser
+import osmosdr
+import time
 import wx
 
 
@@ -37,66 +43,204 @@ class top_block(grc_wxgui.top_block_gui):
         ##################################################
         # Variables
         ##################################################
-        self.samp_rate = samp_rate = 48.5e3
-        self.playback_rate = playback_rate = 1
+        self.sec_delay = sec_delay = 1
+        self.samp_rate = samp_rate = 5e6
+        self.channel_width = channel_width = 200e3
+        self.channel_freq = channel_freq = 96.9e6
+        self.center_freq = center_freq = 97.9e6
+        self.audio_gain = audio_gain = 1
 
         ##################################################
         # Blocks
         ##################################################
-        _playback_rate_sizer = wx.BoxSizer(wx.VERTICAL)
-        self._playback_rate_text_box = forms.text_box(
+        _sec_delay_sizer = wx.BoxSizer(wx.VERTICAL)
+        self._sec_delay_text_box = forms.text_box(
         	parent=self.GetWin(),
-        	sizer=_playback_rate_sizer,
-        	value=self.playback_rate,
-        	callback=self.set_playback_rate,
-        	label='playback_rate',
+        	sizer=_sec_delay_sizer,
+        	value=self.sec_delay,
+        	callback=self.set_sec_delay,
+        	label="Delay (sec)",
         	converter=forms.float_converter(),
         	proportion=0,
         )
-        self._playback_rate_slider = forms.slider(
+        self._sec_delay_slider = forms.slider(
         	parent=self.GetWin(),
-        	sizer=_playback_rate_sizer,
-        	value=self.playback_rate,
-        	callback=self.set_playback_rate,
-        	minimum=.25,
+        	sizer=_sec_delay_sizer,
+        	value=self.sec_delay,
+        	callback=self.set_sec_delay,
+        	minimum=0,
         	maximum=10,
         	num_steps=100,
         	style=wx.SL_HORIZONTAL,
         	cast=float,
         	proportion=1,
         )
-        self.Add(_playback_rate_sizer)
-        self.fractional_interpolator_xx_0 = filter.fractional_interpolator_ff(0, playback_rate)
-        self.blocks_wavfile_sink_0 = blocks.wavfile_sink("radio_recording_2.wav", 1, int(samp_rate), 8)
-        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_float*1, samp_rate,True)
-        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_float*1, "/home/njhazelh/radio_samples", False)
-        (self.blocks_file_source_0).set_min_output_buffer(1000)
-        (self.blocks_file_source_0).set_max_output_buffer(10000)
+        self.Add(_sec_delay_sizer)
+        _channel_freq_sizer = wx.BoxSizer(wx.VERTICAL)
+        self._channel_freq_text_box = forms.text_box(
+        	parent=self.GetWin(),
+        	sizer=_channel_freq_sizer,
+        	value=self.channel_freq,
+        	callback=self.set_channel_freq,
+        	label="Channel Freq (Hz)",
+        	converter=forms.float_converter(),
+        	proportion=0,
+        )
+        self._channel_freq_slider = forms.slider(
+        	parent=self.GetWin(),
+        	sizer=_channel_freq_sizer,
+        	value=self.channel_freq,
+        	callback=self.set_channel_freq,
+        	minimum=87.9e6,
+        	maximum=107.9e6,
+        	num_steps=1000,
+        	style=wx.SL_HORIZONTAL,
+        	cast=float,
+        	proportion=1,
+        )
+        self.Add(_channel_freq_sizer)
+        _audio_gain_sizer = wx.BoxSizer(wx.VERTICAL)
+        self._audio_gain_text_box = forms.text_box(
+        	parent=self.GetWin(),
+        	sizer=_audio_gain_sizer,
+        	value=self.audio_gain,
+        	callback=self.set_audio_gain,
+        	label="Audio Gain",
+        	converter=forms.float_converter(),
+        	proportion=0,
+        )
+        self._audio_gain_slider = forms.slider(
+        	parent=self.GetWin(),
+        	sizer=_audio_gain_sizer,
+        	value=self.audio_gain,
+        	callback=self.set_audio_gain,
+        	minimum=0,
+        	maximum=5,
+        	num_steps=100,
+        	style=wx.SL_HORIZONTAL,
+        	cast=float,
+        	proportion=1,
+        )
+        self.Add(_audio_gain_sizer)
+        self.wxgui_fftsink2_0_0 = fftsink2.fft_sink_c(
+        	self.GetWin(),
+        	baseband_freq=center_freq,
+        	y_per_div=10,
+        	y_divs=10,
+        	ref_level=0,
+        	ref_scale=2.0,
+        	sample_rate=samp_rate,
+        	fft_size=1024,
+        	fft_rate=15,
+        	average=True,
+        	avg_alpha=None,
+        	title="FFT Plot",
+        	peak_hold=False,
+        )
+        self.Add(self.wxgui_fftsink2_0_0.win)
+        self.rational_resampler_xxx_0 = filter.rational_resampler_ccc(
+                interpolation=12,
+                decimation=5,
+                taps=None,
+                fractional_bw=None,
+        )
+        self.osmosdr_source_0 = osmosdr.source( args="numchan=" + str(1) + " " + "" )
+        self.osmosdr_source_0.set_sample_rate(samp_rate)
+        self.osmosdr_source_0.set_center_freq(center_freq, 0)
+        self.osmosdr_source_0.set_freq_corr(0, 0)
+        self.osmosdr_source_0.set_dc_offset_mode(2, 0)
+        self.osmosdr_source_0.set_iq_balance_mode(2, 0)
+        self.osmosdr_source_0.set_gain_mode(False, 0)
+        self.osmosdr_source_0.set_gain(0, 0)
+        self.osmosdr_source_0.set_if_gain(20, 0)
+        self.osmosdr_source_0.set_bb_gain(20, 0)
+        self.osmosdr_source_0.set_antenna("", 0)
+        self.osmosdr_source_0.set_bandwidth(0, 0)
+          
+        self.low_pass_filter_0 = filter.fir_filter_ccf(int(samp_rate / channel_width), firdes.low_pass(
+        	1, samp_rate, 75e3, 25e3, firdes.WIN_HAMMING, 6.76))
+        self.blocks_wavfile_sink_0 = blocks.wavfile_sink("radio_recording.wav", 1, int(48e3), 8)
+        self.blocks_multiply_xx_1 = blocks.multiply_vcc(1)
+        self.blocks_multiply_xx_0 = blocks.multiply_vcc(1)
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_vff((audio_gain, ))
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_float*1, "radio_samples", False)
+        self.blocks_file_sink_0.set_unbuffered(False)
+        self.blocks_delay_0 = blocks.delay(gr.sizeof_gr_complex*1, 480000 * sec_delay)
         self.audio_sink_0 = audio.sink(48000, "", True)
+        self.analog_wfm_rcv_0 = analog.wfm_rcv(
+        	quad_rate=480e3,
+        	audio_decimation=10,
+        )
+        self.analog_sig_source_x_0 = analog.sig_source_c(samp_rate, analog.GR_COS_WAVE, center_freq - channel_freq, 1, 0)
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.blocks_file_source_0, 0), (self.blocks_throttle_0, 0))    
-        self.connect((self.blocks_throttle_0, 0), (self.fractional_interpolator_xx_0, 0))    
-        self.connect((self.fractional_interpolator_xx_0, 0), (self.audio_sink_0, 0))    
-        self.connect((self.fractional_interpolator_xx_0, 0), (self.blocks_wavfile_sink_0, 0))    
+        self.connect((self.analog_sig_source_x_0, 0), (self.blocks_multiply_xx_0, 1))    
+        self.connect((self.analog_wfm_rcv_0, 0), (self.blocks_multiply_const_vxx_0, 0))    
+        self.connect((self.blocks_delay_0, 0), (self.blocks_multiply_xx_1, 0))    
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.audio_sink_0, 0))    
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.blocks_file_sink_0, 0))    
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.blocks_wavfile_sink_0, 0))    
+        self.connect((self.blocks_multiply_xx_0, 0), (self.low_pass_filter_0, 0))    
+        self.connect((self.blocks_multiply_xx_1, 0), (self.analog_wfm_rcv_0, 0))    
+        self.connect((self.low_pass_filter_0, 0), (self.rational_resampler_xxx_0, 0))    
+        self.connect((self.osmosdr_source_0, 0), (self.blocks_multiply_xx_0, 0))    
+        self.connect((self.osmosdr_source_0, 0), (self.wxgui_fftsink2_0_0, 0))    
+        self.connect((self.rational_resampler_xxx_0, 0), (self.blocks_delay_0, 0))    
+        self.connect((self.rational_resampler_xxx_0, 0), (self.blocks_multiply_xx_1, 1))    
+
+    def get_sec_delay(self):
+        return self.sec_delay
+
+    def set_sec_delay(self, sec_delay):
+        self.sec_delay = sec_delay
+        self._sec_delay_slider.set_value(self.sec_delay)
+        self._sec_delay_text_box.set_value(self.sec_delay)
+        self.blocks_delay_0.set_dly(480000 * self.sec_delay)
 
     def get_samp_rate(self):
         return self.samp_rate
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.blocks_throttle_0.set_sample_rate(self.samp_rate)
+        self.analog_sig_source_x_0.set_sampling_freq(self.samp_rate)
+        self.wxgui_fftsink2_0_0.set_sample_rate(self.samp_rate)
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, 75e3, 25e3, firdes.WIN_HAMMING, 6.76))
+        self.osmosdr_source_0.set_sample_rate(self.samp_rate)
 
-    def get_playback_rate(self):
-        return self.playback_rate
+    def get_channel_width(self):
+        return self.channel_width
 
-    def set_playback_rate(self, playback_rate):
-        self.playback_rate = playback_rate
-        self.fractional_interpolator_xx_0.set_interp_ratio(self.playback_rate)
-        self._playback_rate_slider.set_value(self.playback_rate)
-        self._playback_rate_text_box.set_value(self.playback_rate)
+    def set_channel_width(self, channel_width):
+        self.channel_width = channel_width
+
+    def get_channel_freq(self):
+        return self.channel_freq
+
+    def set_channel_freq(self, channel_freq):
+        self.channel_freq = channel_freq
+        self._channel_freq_slider.set_value(self.channel_freq)
+        self._channel_freq_text_box.set_value(self.channel_freq)
+        self.analog_sig_source_x_0.set_frequency(self.center_freq - self.channel_freq)
+
+    def get_center_freq(self):
+        return self.center_freq
+
+    def set_center_freq(self, center_freq):
+        self.center_freq = center_freq
+        self.analog_sig_source_x_0.set_frequency(self.center_freq - self.channel_freq)
+        self.wxgui_fftsink2_0_0.set_baseband_freq(self.center_freq)
+        self.osmosdr_source_0.set_center_freq(self.center_freq, 0)
+
+    def get_audio_gain(self):
+        return self.audio_gain
+
+    def set_audio_gain(self, audio_gain):
+        self.audio_gain = audio_gain
+        self._audio_gain_slider.set_value(self.audio_gain)
+        self._audio_gain_text_box.set_value(self.audio_gain)
+        self.blocks_multiply_const_vxx_0.set_k((self.audio_gain, ))
 
 
 def main(top_block_cls=top_block, options=None):

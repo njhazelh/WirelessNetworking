389f6c223065e7e88c0bca757161d76a0097913a
Phaelyn Kotuby <kotubyp@ccs.neu.edu>
2016-03-04 12:07:39 -0500

    Some progress made on qpsk

--- ps3/prob3/psk_test.py
+++ ps3/prob3/psk_test.py
@@ -2,7 +2,7 @@
 ##################################################
 # Gnuradio Python Flow Graph
 # Title: Psk Test
-# Generated: Thu Mar  3 13:57:51 2016
+# Generated: Fri Mar  4 12:04:38 2016
 ##################################################
 
 from PyQt4 import Qt
@@ -50,7 +50,7 @@ class psk_test(gr.top_block, Qt.QWidget):
         # Variables
         ##################################################
         self.samp_rate = samp_rate = 1e6
-        self.my_constellation = my_constellation = digital.constellation_calcdist(([-1+0j, 1+0j]), ([0, 1]), 2, 1).base()
+        self.my_constellation = my_constellation = digital.constellation_calcdist((complex(0, 0), complex(0, 1), complex(1, 0), complex(1, 1)), (00, 01, 10, 11), 2, 2).base()
 
         ##################################################
         # Blocks
@@ -78,28 +78,26 @@ class psk_test(gr.top_block, Qt.QWidget):
         self._qtgui_const_sink_x_0_win = sip.wrapinstance(self.qtgui_const_sink_x_0.pyqwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_const_sink_x_0_win)
         self.digital_constellation_decoder_cb_0 = digital.constellation_decoder_cb(my_constellation)
-        self.digital_chunks_to_symbols_xx_0 = digital.chunks_to_symbols_bc((-1, 1), 1)
+        self.digital_chunks_to_symbols_xx_0 = digital.chunks_to_symbols_bc((complex(0, 0), complex(0, 1), complex(1, 0), complex(1, 1)), 1)
         self.blocks_throttle_0 = blocks.throttle(gr.sizeof_char*1, samp_rate)
         self.blocks_head_0_0 = blocks.head(gr.sizeof_char*1, 2000000)
         self.blocks_head_0 = blocks.head(gr.sizeof_char*1, 2000000)
-        self.blocks_file_sink_0_0 = blocks.file_sink(gr.sizeof_char*1, "BPSK_in", False)
+        self.blocks_file_sink_0_0 = blocks.file_sink(gr.sizeof_char*1, "QPSK_in", False)
         self.blocks_file_sink_0_0.set_unbuffered(True)
-        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_char*1, "BPSK_out", False)
+        self.blocks_file_sink_0 = blocks.file_sink(gr.sizeof_char*1, "QPSK_out", False)
         self.blocks_file_sink_0.set_unbuffered(True)
         self.blocks_add_xx_0 = blocks.add_vcc(1)
         self.blks2_error_rate_0 = grc_blks2.error_rate(
         	type='BER',
         	win_size=1000,
-        	bits_per_symbol=1,
+        	bits_per_symbol=2,
         )
         self.analog_random_source_x_0 = blocks.vector_source_b(map(int, numpy.random.randint(0, 2, 1000)), True)
-        self.analog_noise_source_x_0 = analog.noise_source_c(analog.GR_GAUSSIAN, 0.5, 0)
+        self.analog_noise_source_x_0 = analog.noise_source_c(analog.GR_GAUSSIAN, 0.1, 0)
 
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.digital_chunks_to_symbols_xx_0, 0), (self.blocks_add_xx_0, 0))
-        self.connect((self.blocks_throttle_0, 0), (self.digital_chunks_to_symbols_xx_0, 0))
         self.connect((self.analog_noise_source_x_0, 0), (self.blocks_add_xx_0, 1))
         self.connect((self.analog_random_source_x_0, 0), (self.blocks_throttle_0, 0))
         self.connect((self.digital_constellation_decoder_cb_0, 0), (self.blks2_error_rate_0, 1))
@@ -109,8 +107,10 @@ class psk_test(gr.top_block, Qt.QWidget):
         self.connect((self.blocks_head_0, 0), (self.blocks_file_sink_0, 0))
         self.connect((self.blocks_head_0_0, 0), (self.blocks_file_sink_0_0, 0))
         self.connect((self.analog_random_source_x_0, 0), (self.blks2_error_rate_0, 0))
-        self.connect((self.blocks_throttle_0, 0), (self.blocks_head_0, 0))
-        self.connect((self.digital_constellation_decoder_cb_0, 0), (self.blocks_head_0_0, 0))
+        self.connect((self.digital_constellation_decoder_cb_0, 0), (self.blocks_head_0, 0))
+        self.connect((self.digital_chunks_to_symbols_xx_0, 0), (self.blocks_add_xx_0, 0))
+        self.connect((self.blocks_throttle_0, 0), (self.digital_chunks_to_symbols_xx_0, 0))
+        self.connect((self.blocks_throttle_0, 0), (self.blocks_head_0_0, 0))
 
 
 # QT sink close method reimplementation

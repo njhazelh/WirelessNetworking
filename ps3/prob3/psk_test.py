0723cb3c5d8676db3ef0cc61a514d8d030b65182
njhazelh <njhazelh@gmail.com>
2016-03-06 00:23:38 -0500

    Patch constellation object correctly for QPSK

--- ps3/prob3/psk_test.py
+++ ps3/prob3/psk_test.py
@@ -1,9 +1,20 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
 ##################################################
-# Gnuradio Python Flow Graph
+# GNU Radio Python Flow Graph
 # Title: Psk Test
-# Generated: Fri Mar  4 13:10:57 2016
+# Generated: Sun Mar  6 00:01:42 2016
 ##################################################
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
 
 from PyQt4 import Qt
 from gnuradio import analog
@@ -20,6 +31,7 @@ import numpy
 import sip
 import sys
 
+
 class psk_test(gr.top_block, Qt.QWidget):
 
     def __init__(self):
@@ -27,9 +39,9 @@ class psk_test(gr.top_block, Qt.QWidget):
         Qt.QWidget.__init__(self)
         self.setWindowTitle("Psk Test")
         try:
-             self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
         except:
-             pass
+            pass
         self.top_scroll_layout = Qt.QVBoxLayout()
         self.setLayout(self.top_scroll_layout)
         self.top_scroll = Qt.QScrollArea()
@@ -45,12 +57,11 @@ class psk_test(gr.top_block, Qt.QWidget):
         self.settings = Qt.QSettings("GNU Radio", "psk_test")
         self.restoreGeometry(self.settings.value("geometry").toByteArray())
 
-
         ##################################################
         # Variables
         ##################################################
         self.samp_rate = samp_rate = 1e6
-        self.my_constellation = my_constellation = digital.constellation_calcdist((complex(0, 0), complex(0, 1), complex(1, 0), complex(1, 1)), (00, 01, 10, 11), 2, 1).base()
+        self.my_constellation = my_constellation = digital.constellation_calcdist((complex(0, 1), complex(1, 0), complex(0, 0), complex(1, 1)), (00, 01, 10, 11), 2, 2).base()
 
         ##################################################
         # Blocks
@@ -63,8 +74,42 @@ class psk_test(gr.top_block, Qt.QWidget):
         )
         self.qtgui_time_sink_x_0.set_update_time(0.10)
         self.qtgui_time_sink_x_0.set_y_axis(-1, 1)
+        
+        self.qtgui_time_sink_x_0.set_y_label("Amplitude", "")
+        
         self.qtgui_time_sink_x_0.enable_tags(-1, True)
         self.qtgui_time_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, 0, "")
+        self.qtgui_time_sink_x_0.enable_autoscale(False)
+        self.qtgui_time_sink_x_0.enable_grid(False)
+        self.qtgui_time_sink_x_0.enable_control_panel(False)
+        
+        if not True:
+          self.qtgui_time_sink_x_0.disable_legend()
+        
+        labels = ["", "", "", "", "",
+                  "", "", "", "", ""]
+        widths = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        colors = ["blue", "red", "green", "black", "cyan",
+                  "magenta", "yellow", "dark red", "dark green", "blue"]
+        styles = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        markers = [-1, -1, -1, -1, -1,
+                   -1, -1, -1, -1, -1]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
+        
+        for i in xrange(1):
+            if len(labels[i]) == 0:
+                self.qtgui_time_sink_x_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_time_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_time_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_time_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_time_sink_x_0.set_line_style(i, styles[i])
+            self.qtgui_time_sink_x_0.set_line_marker(i, markers[i])
+            self.qtgui_time_sink_x_0.set_line_alpha(i, alphas[i])
+        
         self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.pyqwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_time_sink_x_0_win)
         self.qtgui_const_sink_x_0 = qtgui.const_sink_c(
@@ -75,11 +120,41 @@ class psk_test(gr.top_block, Qt.QWidget):
         self.qtgui_const_sink_x_0.set_update_time(0.10)
         self.qtgui_const_sink_x_0.set_y_axis(-2, 2)
         self.qtgui_const_sink_x_0.set_x_axis(-2, 2)
+        self.qtgui_const_sink_x_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, qtgui.TRIG_SLOPE_POS, 0.0, 0, "")
+        self.qtgui_const_sink_x_0.enable_autoscale(False)
+        self.qtgui_const_sink_x_0.enable_grid(False)
+        
+        if not True:
+          self.qtgui_const_sink_x_0.disable_legend()
+        
+        labels = ["", "", "", "", "",
+                  "", "", "", "", ""]
+        widths = [1, 1, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        colors = ["blue", "red", "red", "red", "red",
+                  "red", "red", "red", "red", "red"]
+        styles = [0, 0, 0, 0, 0,
+                  0, 0, 0, 0, 0]
+        markers = [0, 0, 0, 0, 0,
+                   0, 0, 0, 0, 0]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
+        for i in xrange(1):
+            if len(labels[i]) == 0:
+                self.qtgui_const_sink_x_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_const_sink_x_0.set_line_label(i, labels[i])
+            self.qtgui_const_sink_x_0.set_line_width(i, widths[i])
+            self.qtgui_const_sink_x_0.set_line_color(i, colors[i])
+            self.qtgui_const_sink_x_0.set_line_style(i, styles[i])
+            self.qtgui_const_sink_x_0.set_line_marker(i, markers[i])
+            self.qtgui_const_sink_x_0.set_line_alpha(i, alphas[i])
+        
         self._qtgui_const_sink_x_0_win = sip.wrapinstance(self.qtgui_const_sink_x_0.pyqwidget(), Qt.QWidget)
         self.top_layout.addWidget(self._qtgui_const_sink_x_0_win)
         self.digital_constellation_decoder_cb_0 = digital.constellation_decoder_cb(my_constellation)
-        self.digital_chunks_to_symbols_xx_0 = digital.chunks_to_symbols_bc((complex(0, 0), complex(0, 1), complex(1, 0), complex(1, 1)), 1)
-        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_char*1, samp_rate)
+        self.digital_chunks_to_symbols_xx_0 = digital.chunks_to_symbols_bc((my_constellation.points()), my_constellation.dimensionality())
+        self.blocks_throttle_0 = blocks.throttle(gr.sizeof_char*1, samp_rate,True)
         self.blocks_head_0_0 = blocks.head(gr.sizeof_char*1, 2000000)
         self.blocks_head_0 = blocks.head(gr.sizeof_char*1, 2000000)
         self.blocks_file_sink_0_0 = blocks.file_sink(gr.sizeof_char*1, "QPSK_in", False)
@@ -98,60 +173,60 @@ class psk_test(gr.top_block, Qt.QWidget):
         ##################################################
         # Connections
         ##################################################
-        self.connect((self.analog_random_source_x_0, 0), (self.blocks_throttle_0, 0))
-        self.connect((self.digital_constellation_decoder_cb_0, 0), (self.blks2_error_rate_0, 1))
-        self.connect((self.blocks_add_xx_0, 0), (self.qtgui_const_sink_x_0, 0))
-        self.connect((self.blocks_add_xx_0, 0), (self.digital_constellation_decoder_cb_0, 0))
-        self.connect((self.blks2_error_rate_0, 0), (self.qtgui_time_sink_x_0, 0))
-        self.connect((self.blocks_head_0, 0), (self.blocks_file_sink_0, 0))
-        self.connect((self.blocks_head_0_0, 0), (self.blocks_file_sink_0_0, 0))
-        self.connect((self.analog_random_source_x_0, 0), (self.blks2_error_rate_0, 0))
-        self.connect((self.blocks_throttle_0, 0), (self.blocks_head_0_0, 0))
-        self.connect((self.digital_constellation_decoder_cb_0, 0), (self.blocks_head_0, 0))
-        self.connect((self.blocks_throttle_0, 0), (self.digital_chunks_to_symbols_xx_0, 0))
-        self.connect((self.digital_chunks_to_symbols_xx_0, 0), (self.blocks_add_xx_0, 0))
-        self.connect((self.analog_noise_source_x_0, 0), (self.blocks_add_xx_0, 1))
+        self.connect((self.analog_noise_source_x_0, 0), (self.blocks_add_xx_0, 1))    
+        self.connect((self.analog_random_source_x_0, 0), (self.blks2_error_rate_0, 0))    
+        self.connect((self.analog_random_source_x_0, 0), (self.blocks_throttle_0, 0))    
+        self.connect((self.blks2_error_rate_0, 0), (self.qtgui_time_sink_x_0, 0))    
+        self.connect((self.blocks_add_xx_0, 0), (self.digital_constellation_decoder_cb_0, 0))    
+        self.connect((self.blocks_add_xx_0, 0), (self.qtgui_const_sink_x_0, 0))    
+        self.connect((self.blocks_head_0, 0), (self.blocks_file_sink_0, 0))    
+        self.connect((self.blocks_head_0_0, 0), (self.blocks_file_sink_0_0, 0))    
+        self.connect((self.blocks_throttle_0, 0), (self.blocks_head_0_0, 0))    
+        self.connect((self.blocks_throttle_0, 0), (self.digital_chunks_to_symbols_xx_0, 0))    
+        self.connect((self.digital_chunks_to_symbols_xx_0, 0), (self.blocks_add_xx_0, 0))    
+        self.connect((self.digital_constellation_decoder_cb_0, 0), (self.blks2_error_rate_0, 1))    
+        self.connect((self.digital_constellation_decoder_cb_0, 0), (self.blocks_head_0, 0))    
 
-
-# QT sink close method reimplementation
     def closeEvent(self, event):
         self.settings = Qt.QSettings("GNU Radio", "psk_test")
         self.settings.setValue("geometry", self.saveGeometry())
         event.accept()
+
 
     def get_samp_rate(self):
         return self.samp_rate
 
     def set_samp_rate(self, samp_rate):
         self.samp_rate = samp_rate
-        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
         self.blocks_throttle_0.set_sample_rate(self.samp_rate)
+        self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate)
 
     def get_my_constellation(self):
         return self.my_constellation
 
     def set_my_constellation(self, my_constellation):
         self.my_constellation = my_constellation
+        self.digital_chunks_to_symbols_xx_0.set_symbol_table((self.my_constellation.points()))
 
-if __name__ == '__main__':
-    import ctypes
-    import os
-    if os.name == 'posix':
-        try:
-            x11 = ctypes.cdll.LoadLibrary('libX11.so')
-            x11.XInitThreads()
-        except:
-            print "Warning: failed to XInitThreads()"
-    parser = OptionParser(option_class=eng_option, usage="%prog: [options]")
-    (options, args) = parser.parse_args()
+
+def main(top_block_cls=psk_test, options=None):
+
+    from distutils.version import StrictVersion
+    if StrictVersion(Qt.qVersion()) >= StrictVersion("4.5.0"):
+        style = gr.prefs().get_string('qtgui', 'style', 'raster')
+        Qt.QApplication.setGraphicsSystem(style)
     qapp = Qt.QApplication(sys.argv)
-    tb = psk_test()
+
+    tb = top_block_cls()
     tb.start()
     tb.show()
+
     def quitting():
         tb.stop()
         tb.wait()
     qapp.connect(qapp, Qt.SIGNAL("aboutToQuit()"), quitting)
     qapp.exec_()
-    tb = None #to clean up Qt widgets
 
+
+if __name__ == '__main__':
+    main()
